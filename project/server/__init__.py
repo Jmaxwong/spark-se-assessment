b9d205d7872638c4fd27ac9924fffc713bc31350
Jmaxwong <68025573+Jmaxwong@users.noreply.github.com>
2022-01-31 22:29:03 -0500

    test

--- project/server/__init__.py
+++ project/server/__init__.py
@@ -34,10 +34,12 @@ db = SQLAlchemy(app)
 from project.server.models import User
 migrate = Migrate(app, db)
 
-
 @app.route("/")
 def root_site():
     return "<p>It works!</p>"
+
+from project.server.auth.views import auth_blueprint
+app.register_blueprint(auth_blueprint)
 
 @app.cli.command()
 @click.option('--coverage/--no-coverage', default=False,
