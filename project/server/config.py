4280b8a7604f3e5210ba66d68428f5c44542094d
Jmaxwong <68025573+Jmaxwong@users.noreply.github.com>
2022-02-01 19:40:50 -0500

    Added config to heroku uri

--- project/server/config.py
+++ project/server/config.py
@@ -1,7 +1,10 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
-postgres_local_base = 'postgresql:///' #'sqlite:///'  
+postgres_local_base = 'postgresql:///' #'sqlite:///' 
 database_name = 'diagnostic'
+user = 'pwcbilvuomyhps'
+password = '9b7848cd8db818bde579bb007b73a11ba025ada7a6ce377963f58d7c85832f3c'
+alch_uri = 'postgres://pwcbilvuomyhps:9b7848cd8db818bde579bb007b73a11ba025ada7a6ce377963f58d7c85832f3c@ec2-107-20-153-39.compute-1.amazonaws.com:5432/dbn5l2onl9kdq4'
 
 
 class BaseConfig:
@@ -16,7 +19,8 @@ class DevelopmentConfig(BaseConfig):
     """Development configuration."""
     DEBUG = True
     BCRYPT_LOG_ROUNDS = 4
-    SQLALCHEMY_DATABASE_URI = postgres_local_base + database_name
+    SQLALCHEMY_DATABASE_URI = alch_uri
+
 
 
 class TestingConfig(BaseConfig):
@@ -24,7 +28,7 @@ class TestingConfig(BaseConfig):
     DEBUG = True
     TESTING = True
     BCRYPT_LOG_ROUNDS = 4
-    SQLALCHEMY_DATABASE_URI = postgres_local_base + database_name + '_test.db'
+    SQLALCHEMY_DATABASE_URI = alch_uri
     PRESERVE_CONTEXT_ON_EXCEPTION = False
 
 
@@ -32,4 +36,4 @@ class ProductionConfig(BaseConfig):
     """Production configuration."""
     SECRET_KEY = 'diagnostic_secret'
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = 'postgresql:///diagnostic' #'sqlite:///diagnostic' 
+    SQLALCHEMY_DATABASE_URI = alch_uri
